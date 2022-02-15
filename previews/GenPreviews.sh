5d6f81878b911eba294104f57af408962e362eb8
Brian Enigma <brian@netninja.com>
2022-02-15 09:42:53 -0800

    Fixes to previews
    - HTML reflects current camera set
    - Kill any old ffmpeg processes that might have gotten stuck. Apparently
      macOS's systemd feature to kill a timer-initiated process if it's been
      running too long doesn't correctly kill this (because subprocess?)

--- previews/GenPreviews.sh
+++ previews/GenPreviews.sh
@@ -2,21 +2,27 @@
 
 source config.sh
 
+# Kill any previous instance that might be around. Systemd doesn't seem to correctly timeout.
+ps ax | grep ffmpeg | grep thumbnail | sed 's/^ *//' | cut -d ' ' -f1 | xargs kill
+
+# Capture thumbnails to temp folder
 for ((i = 0 ; i < ${#CAMERAS[@]} ; i++)) ; do
 	LINE=(${CAMERAS[i]})
 	IP=${LINE[0]}
 	NAME=${LINE[1]}
 	USER=${LINE[2]}
 	PASS=${LINE[3]}
-	ffmpeg -hide_banner -loglevel fatal \
+	ffmpeg -hide_banner \
 		-i rtsp://$USER:$PASS@$IP:554/h264Preview_01_sub \
 		-vf "select='eq(pict_type,PICT_TYPE_I)'" \
 		-vsync vfr \
 		-frames 1 \
 		-f image2 \
 		-y /Users/admin/Security/live/thumbnail-$NAME.jpg
+	#ffmpeg -hide_banner -loglevel fatal \
 done
 
+# Copy to web folder
 cp /Users/admin/Security/live/thumbnail* /Users/admin/Security/recent/
 
 exit 0
